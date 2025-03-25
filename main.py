65f33e4dc401a5b1282bd2281173d3e9015a1fcf
Gordon Chen <gordonc1@sas.upenn.edu>
2025-03-25 13:07:31 -0400

    feat: implement password validation checks in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flask
 
 
 # TODO: change this to your academic email
-AUTHOR = "lumbroso@seas.upenn.edu"
+AUTHOR = "gordonc1@sas.upenn.edu"
 
 
 app = flask.Flask(__name__)
@@ -25,5 +25,17 @@ def check_password():
     data = flask.request.get_json() or {}
     pw = data.get("password", "")
 
-    # FIXME: to be implemented
-    return flask.jsonify({"valid": False, "reason": "Not implemented"}), 501
+    # check if password length is valid 
+    if len(pw) < 8:
+        return flask.jsonify({"valid": False, "reason": "Password is too short"}), 400
+    # check if at least 1 uppercase letter 
+    if not any(x.isupper() for x in pw):
+        return flask.jsonify({"valid": False, "reason": "Password does not contain an uppercase letter"}), 400
+    # check if at least 1 digit 
+    if not any(x.isdigit() for x in pw):
+        return flask.jsonify({"valid": False, "reason": "Password does not contain a digit"}), 400
+    # check if at least 1 special character
+    if not any(not x.isalnum() for x in pw):
+        return flask.jsonify({"valid": False, "reason": "Password does not contain a special character"}), 400
+    else:
+        return flask.jsonify({"valid": True, "reason": "is valid"})
